d341aa6cf6e6a3ef937f76c96ed1003ceb03f9b7
Talha Yigithan Doganer <tdoganerr@gmail.com>
2026-01-26 21:07:24 +0200

    Supabase anahtar ismi duzeltildi

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+// AŞAĞIDAKİ SATIR DEĞİŞTİ (ANON EKLENDİ):
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
